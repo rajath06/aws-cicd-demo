a867a2fe33fae2643a2c1506badf8845415ece9e
rajath06 <83548992+rajath06@users.noreply.github.com>
2024-03-11 16:28:10 +0530

    add the filed for code deploy

--- python-app/scripts/start_container.sh
+++ python-app/scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+echo pull rajath06/sample-python-app
 
 # Run the Docker image as a container
-echo
+echo run -d -p 5000:5000 rajath06/sample-python-app
